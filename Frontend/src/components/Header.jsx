b1251cbce1846a1d5a6f75530b8a66f493b25376
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-07-16 16:53:20 +0530

    Added the navigation in the header

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Link as MuiLink } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -10,9 +11,35 @@ const Header = () => {
             URL Shortener
           </Typography>
         </Box>
-        <Typography variant="body1" sx={{ fontStyle: 'italic' }}>
-          Made by Karthick Prabakaran
-        </Typography>
+
+        {/* Centered Links */}
+        <Box sx={{ display: 'flex', gap: 3, justifyContent: 'center', flexGrow: 1 }}>
+          <MuiLink
+            component={Link}
+            to="/"
+            color="inherit"
+            underline="none"
+            sx={{ fontWeight: 'bold' }}
+          >
+            Shorten URL
+          </MuiLink>
+          <MuiLink
+            component={Link}
+            to="/stats/"
+            color="inherit"
+            underline="none"
+            sx={{ fontWeight: 'bold' }}
+          >
+            Stats
+          </MuiLink>
+        </Box>
+
+        {/* Right-Aligned Author Name */}
+        <Box sx={{ flexGrow: 1, textAlign: 'right' }}>
+          <Typography variant="body1" sx={{ fontStyle: 'italic' }}>
+            Made by Karthick Prabakaran
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
